11f8ad2b4dcbede3c9aab97d344055256d22204d
vitkr <vitkr@VITPC>
2025-02-27 13:28:10 +0100

    Add project files.

---
+++ PGM2_TEST/Models/Koule.cs
@@ -0,0 +1,25 @@
+﻿using PGM2_TEST.Pages;
+
+namespace PGM2_TEST.Models
+{
+    public class Koule
+    {
+        private int polomer;
+        public int Polomer
+        {
+            get => polomer;
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    polomer = value;
+                }
+            }
+        }
+
+        public double Povrch => 4 * Math.PI * Math.Pow(Polomer, 2);
+
+        public double Objem => 4 / 3 * Math.PI * Math.Pow(Polomer, 3);
+
+    }
+}
